0a0b52f7a92d3af13b9b887437ba082bd3496f39
BenjiBaruch <80351136+BenjiBaruch@users.noreply.github.com>
2024-03-01 12:43:46 -0800

    fixed svg rendering glitch, home/end well glitches, and added winner detection

--- src/App.js
+++ src/App.js
@@ -7,14 +7,15 @@ import DiceRow from './DiceRow';
 
 
 function App() {
+  const piecesPerSide = 7
   const [activePlayer, setActivePlayer] = useState(0);
   const [diceValues, setDiceValues] = useState([Array(4).fill(0)]);
   const [diceScore, setDiceScore] = useState(0);
   const [redBoard, setRedBoard] = useState([Array(15).fill(false)]);
   const [blueBoard, setBlueBoard] = useState([Array(15).fill(false)]);
-  const [redHomeWell, setRedHomeWell] = useState(7);
+  const [redHomeWell, setRedHomeWell] = useState(piecesPerSide);
   const [redEndWell, setRedEndWell] = useState(0);
-  const [blueHomeWell, setBlueHomeWell] = useState(7);
+  const [blueHomeWell, setBlueHomeWell] = useState(piecesPerSide);
   const [blueEndWell, setBlueEndWell] = useState(0);
   const [winner, setWinner] = useState(-1);
   const [highlight, setHighlight] = useState(-1);
@@ -30,14 +31,20 @@ function App() {
     setDiceValues(vals);
   }
 
+  function checkWinner() {
+    if (redEndWell === piecesPerSide) {
+      setWinner(0);
+      setHighlight(-1);
+    }
+    else if (blueEndWell === piecesPerSide) {
+      setWinner(1);
+      setHighlight(-1);
+    }
+  }
+
   function doTurn(square, row) {
     console.log("doTurn(" + square + ')');
     console.log("dice score type: " + typeof diceScore);
-
-    if (square === -1) {
-      console.log("invalid square");
-      return;
-    }
 
     if (diceScore === 0) {
       console.log("zero dice");
@@ -46,28 +53,51 @@ function App() {
       return;
     }
 
-    const [activeHomeWell, setActiveHomeWell, activeEndWell, setActiveEndWell] = (activePlayer === 0) 
-      ? [redHomeWell, setRedHomeWell, redEndWell, setRedEndWell]
-      : [blueHomeWell, setBlueHomeWell, blueEndWell, setBlueEndWell];
+    const [activeHomeWell, setActiveHomeWell, activeEndWell, setActiveEndWell, opponentHomeWell, setOpponentHomeWell] = (activePlayer === 0) 
+      ? [redHomeWell, setRedHomeWell, redEndWell, setRedEndWell, blueHomeWell, setBlueHomeWell]
+      : [blueHomeWell, setBlueHomeWell, blueEndWell, setBlueEndWell, redHomeWell, setRedHomeWell];
 
-
-    if ((row === "_red" && activePlayer === 1) || (row === "_blue" && activePlayer === 0)) {
-      console.log("wrong side");
-      return;
-    }
-    
     const dest = square + diceScore;
-    if (dest > 15) {
-      console.log("dest too far");
-      return;
-    }
+
     const finish = dest === 15;
     const rosette = dest === 4 || dest === 8 || dest === 14;
     const raceRow = 4 < dest && dest < 13;
 
     const [activeBoard, setActiveBoard] = (activePlayer === 0) ? [redBoard.slice(), setRedBoard] : [blueBoard.slice(), setBlueBoard];
     const [opponentBoard, setOpponentBoard] = (activePlayer === 1) ? [redBoard.slice(), setRedBoard] : [blueBoard.slice(), setBlueBoard];
-  
+
+    let movePossible = false;
+    for (let i = 0; i < 15; i++) {
+      if (activeBoard[i] || (i == 0 && activeHomeWell > 0)) {
+        let localDest = i + diceScore;
+        if (!((activeBoard[localDest] && localDest < 15) || (localDest == 8 && opponentBoard[8]) || localDest > 15)) {
+          movePossible = true;
+          break;
+        }
+      }
+    }
+    if (!movePossible) {
+      console.log("no possible move");
+      setActivePlayer((activePlayer + 1) % 2);
+      rollDice();
+      return;
+    }
+
+    if ((row === "_red" && activePlayer === 1) || (row === "_blue" && activePlayer === 0)) {
+      console.log("wrong side");
+      return;
+    }
+    
+    if (dest > 15) {
+      console.log("dest too far");
+      return;
+    }
+
+    if (square === -1) {
+      console.log("invalid square");
+      return;
+    }
+
     console.log("turn: " + (activePlayer === 0 ? "red" : "blue") + 
               "\nRed home well: " + redHomeWell + 
               "\nBlue home well: " + blueHomeWell + 
@@ -109,7 +139,8 @@ function App() {
     }
 
     activeBoard[dest] = true;
-    if (raceRow) {
+    if (raceRow && opponentBoard[dest]) {
+      setOpponentHomeWell(opponentHomeWell+1);
       opponentBoard[dest] = false;
     }
 
@@ -120,44 +151,91 @@ function App() {
       setActivePlayer((activePlayer + 1) % 2);
     }
     rollDice();
+
+    checkWinner();
+  }
+
+  function doNothing(hehe, lol) {
+    return;
+  }
+  function DONThighlight(comedy) {
+    return;
   }
 
   function mouseHighlight(tile) {
     setHighlight(tile);
   }
 
-  return (
-    <div className="App">
-      <DiceRow occupancy={diceValues} activePlayer={activePlayer} />
-      <HomeRow 
-        occupancy={redBoard} 
-        color="_red" 
-        onClick={doTurn} 
-        onHover={mouseHighlight} 
-        bottom={false} 
-        highlight={highlight} 
-        homeWell={redHomeWell}
-        endWell={redEndWell}
-      />
-      <RaceRow 
-        red={redBoard} 
-        blue={blueBoard} 
-        onClick={doTurn} 
-        onHover={mouseHighlight} 
-        highlight={highlight} 
-      />
-      <HomeRow 
-        occupancy={blueBoard} 
-        color="_blue" 
-        onClick={doTurn} 
-        onHover={mouseHighlight} 
-        bottom={true} 
-        highlight={highlight}
-        homeWell={blueHomeWell}
-        endWell={blueEndWell} 
-      />
-    </div>
-  );
+  if (winner === -1) {
+    return (
+      <div className="App">
+        <DiceRow occupancy={diceValues} activePlayer={activePlayer} />
+        <HomeRow 
+          occupancy={redBoard} 
+          color="_red" 
+          onClick={doTurn} 
+          onHover={mouseHighlight} 
+          bottom={false} 
+          highlight={highlight} 
+          homeWell={redHomeWell}
+          endWell={redEndWell}
+        />
+        <RaceRow 
+          red={redBoard} 
+          blue={blueBoard} 
+          onClick={doTurn} 
+          onHover={mouseHighlight} 
+          highlight={highlight} 
+        />
+        <HomeRow 
+          occupancy={blueBoard} 
+          color="_blue" 
+          onClick={doTurn} 
+          onHover={mouseHighlight} 
+          bottom={true} 
+          highlight={highlight}
+          homeWell={blueHomeWell}
+          endWell={blueEndWell} 
+        />
+      </div>
+    );
+  }
+  else {
+    return (
+      <div className="App">
+        <h1 className="winner">
+          {winner === 0 ? "RED WINS!!!!" : "BLUE WINS!!!" /*blue does not deserve as many exclamation points*/} 
+        </h1>
+        <HomeRow 
+          occupancy={redBoard} 
+          color="_red" 
+          onClick={doNothing} 
+          onHover={DONThighlight} 
+          bottom={false} 
+          highlight={highlight} 
+          homeWell={redHomeWell}
+          endWell={redEndWell}
+        />
+        <RaceRow 
+          red={redBoard} 
+          blue={blueBoard} 
+          onClick={doNothing} 
+          onHover={DONThighlight} 
+          highlight={highlight} 
+        />
+        <HomeRow 
+          occupancy={blueBoard} 
+          color="_blue" 
+          onClick={doNothing} 
+          onHover={DONThighlight} 
+          bottom={true} 
+          highlight={highlight}
+          homeWell={blueHomeWell}
+          endWell={blueEndWell} 
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
